3afb6b66d674f3ede1bd1ba4cd50a22948b4156b
Matt Budz <matt.budz@protonmail.com>
2023-10-26 17:12:21 +0200

    update CTA: target & clear nav item active classes

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -4,11 +4,11 @@ window.onload = function () {
   var nav = document.querySelector('.navbar');
   var navOffset = nav.offsetHeight;
 
-  var links = document.querySelectorAll('.navbar .nav-link');
+  var links = document.querySelectorAll('.navbar .nav-item a');
   links.forEach(function (link) {
     link.addEventListener('click', function (event) {
-      console.log(link);
       var href = this.getAttribute('href');
+      var isCTA = this === document.querySelector('[data-behavior~=nav-cta]');
 
       event.preventDefault();
       window.location.hash = href;
@@ -17,10 +17,25 @@ window.onload = function () {
       window.scrollTo({
         top:
           targetElement.getBoundingClientRect().top +
-          window.pageYOffset -
+          window.scrollY -
           navOffset,
         behavior: 'smooth',
       });
+
+      // Clear active class added to nav-links by BS5 ScrollSpy
+      let lastPos = null;
+      requestAnimationFrame(checkPos);
+      function checkPos() {
+        const newPos = targetElement.getBoundingClientRect().top;
+        if (newPos === lastPos && isCTA) {
+          document.querySelectorAll('.nav-link').forEach(function (link) {
+            link.classList.remove('active');
+          });
+        } else {
+          lastPos = newPos;
+          requestAnimationFrame(checkPos);
+        }
+      }
     });
   });
 
